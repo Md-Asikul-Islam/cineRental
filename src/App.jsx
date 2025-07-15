be3a760aff90d8c0e4358065a89a96dc65e60f72
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-15 11:01:50 +0600

    import image in Header and making responsive sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className='bg-secondary'>App</div>
+    <>
+      <Header />
+    </>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,49 @@
 import React from "react";
+import logo from '../assets/logo.svg'
+import ring from '../assets/ring.svg'
+import moon from '../assets/icons/moon.svg'
+import cart from '../assets/shopping-cart.svg'
 
 function Header() {
   return (
     <header>
-      <nav class="container flex items-center justify-between space-x-10 py-6">
+      <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
-          <img src="./assets/logo.svg" width="139" height="26" alt="" />
+          <img src={logo} alt="logo" />
         </a>
 
-        <ul class="flex items-center space-x-5">
+        <ul className="flex items-center space-x-5">
           <li>
             <a
-              class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img src="./assets/ring.svg" width="24" height="24" alt="" />
+              <img src={ring} alt="ring" />
             </a>
           </li>
           <li>
             <a
-              class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
               <img
-                src="./assets/icons/moon.svg"
+                src={moon}
                 width="24"
                 height="24"
-                alt=""
+                alt="moon"
               />
             </a>
           </li>
           <li>
             <a
-              class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
               <img
-                src="./assets/shopping-cart.svg"
+                src={cart}
                 width="24"
                 height="24"
-                alt=""
+                alt="cart"
               />
             </a>
           </li>
