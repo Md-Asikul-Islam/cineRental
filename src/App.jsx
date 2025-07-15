d16a6151762d74016ed003252eb0fc597c085fe6
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-15 11:10:23 +0600

    import image sidebar and Layout making app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Header from './Components/Header'
+import React from "react";
+import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto ">
       <Header />
-    </>
-  )
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
