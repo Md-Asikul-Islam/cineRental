bd7b5a1a36d48ee2b3577b4bcf1308eb4389645f
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-17 13:09:17 +0600

    create a context for passing movie data

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from "./Components/Header";
 import SideBar from "./Components/SideBar";
 import Footer from './Components/Footer';
 import MovieList from "./movieBox/MovieList";
+import { MovieContext } from "./Context";
 
 function App() {
   return (
     <div className="container mx-auto ">
+      <MovieContext.Provider>
       <Header />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
@@ -15,6 +17,7 @@ function App() {
       </main>
       <MovieList />
       <Footer />
+      </MovieContext.Provider>
     </div>
   );
 }
