d242f8b8f36047b84b08f1956208f11390469ca9
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-17 10:15:33 +0600

    update class to className all of component

--- src/movieBox/MovieCard.jsx
+++ src/movieBox/MovieCard.jsx
@@ -4,19 +4,19 @@ import tag from '../assets/tag.svg'
 import { getImageUrl } from './../utils/utility'; 
 function MovieCard({movie}) {
   return (
-    <figure class="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img class="w-full object-cover" src={getImageUrl(movie.cover)}  alt={movie.title} />
-      <figcaption class="pt-4">
-        <h3 class="text-xl mb-1"> {movie.title} </h3>
-        <p class="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-        <div class="flex items-center space-x-1 mb-5">
+    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+      <img className="w-full object-cover" src={getImageUrl(movie.cover)}  alt={movie.title} />
+      <figcaption className="pt-4">
+        <h3 className="text-xl mb-1"> {movie.title} </h3>
+        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+        <div className="flex items-center space-x-1 mb-5">
           <img src={star} width="14" height="14" alt="star" />
           <img src={star} width="14" height="14" alt="star" />
           <img src={star} width="14" height="14" alt="star" />
           <img src={star} width="14" height="14" alt="star" />
         </div>
         <a
-          class="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
           href="#"
         >
           <img src={tag} alt="tag" />
--- src/movieBox/MovieList.jsx
+++ src/movieBox/MovieList.jsx
@@ -5,8 +5,8 @@ import { getAllMovies } from "../data";
 function MovieList() {
   const movies = getAllMovies();
   return (
-    <div class="content">
-      <div class="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
+    <div className="content">
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
         {movies.map((movie) => (
           <MovieCard movie={movie} />
         ))}
