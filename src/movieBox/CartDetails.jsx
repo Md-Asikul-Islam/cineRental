0b4def246ea554b989b27f75ab7f1c85dc3c6ec5
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-19 11:04:57 +0600

    Render dynamic cart items with remove functionality in CartDetails

--- src/movieBox/CartDetails.jsx
+++ src/movieBox/CartDetails.jsx
@@ -3,8 +3,17 @@ import del from "../assets/delete.svg";
 import checkout from "../assets/icons/checkout.svg";
 import { MovieContext } from "../Context";
 import { getImageUrl } from "../utils/utility";
-function CartDetails({ onCloseCart }) {
+function CartDetails({ onCloseCart}) {
   const { cartData, setCartData } = useContext(MovieContext);
+  
+  const handleDeleteItem = (e, itemId) => {
+    e.preventDefault()
+    const filterItem = cartData.filter((item) => {
+      return item.id !== itemId 
+    })
+
+    setCartData([...filterItem])
+  }
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
@@ -13,8 +22,10 @@ function CartDetails({ onCloseCart }) {
             Your Carts
           </h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-            {cartData.map((item) => (
-              <div className="grid grid-cols-[1fr_auto] gap-4">
+            {
+            cartData.length === 0 ? (<p className="text-xl text-red-600 font-semibold "> The cart is empty</p>)  :
+            (cartData.map((item) => (
+              <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
                 <div className="flex items-center gap-4">
                   <img
                     className="rounded overflow-hidden "
@@ -32,13 +43,16 @@ function CartDetails({ onCloseCart }) {
                   </div>
                 </div>
                 <div className="flex justify-between gap-4 items-center">
-                  <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
+                  <button 
+                   onClick={(e) => handleDeleteItem(e, item.id)}
+                  className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
                     <img className="w-5 h-5" src={del} alt="del" />
                     <span className="max-md:hidden">Remove</span>
                   </button>
                 </div>
               </div>
-            ))}
+            )))
+          }
           </div>
           <div className="flex items-center justify-end gap-2">
             <a
